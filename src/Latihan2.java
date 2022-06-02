1b91feb5c23f19f2b72e55515a5922170ca26564
fachrul-dev <fachrul.dev99@gmail.com>
2022-06-02 13:26:13 +0700

    latihan 2

---
+++ src/Latihan2.java
@@ -0,0 +1,6 @@
+public class Latihan2 {
+
+    public static void main(String[] args) {
+//        Tes
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,46 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 50; i++) {
-            if (i % 3 == 0) {
-                System.out.println("n habis dibagi 3");
-            } else if (i % 5 == 0) {
-                System.out.println("n habis dibagi 5");
+
+//        for (int i = 1; i <= 50; i++) {
+//            if (i % 3 == 0) {
+//                System.out.println("n habis dibagi 3");
+//            } else if (i % 5 == 0) {
+//                System.out.println("n habis dibagi 5");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+        for (int i = 1; i <= 20; i++) {
+            if (i % 1 == 2) {
+                System.out.println(i + " bukan bilangan prima");
+
             } else {
-                System.out.println(i);
+                System.out.println(i + " bilangan prima");
             }
         }
+
+
+//        int input = 20;
+//        int bil, cek=0;
+//
+//        System.out.println("PROGRAM JAVA MENENTUKAN BILANGAN PRIMA ATAU BUKAN");
+//        System.out.print("Masukan Angka : ");
+//        bil=input;
+//
+//        System.out.println("----------------------------------------------");
+//        for (int i=2; i<=bil; i++){
+//            if (bil%i==0){
+//                cek++;
+//            }
+//        }
+//
+//        if (cek==1){
+//            System.out.println(bil+" adalah bilangan prima");
+//        }else {
+//            System.out.println(bil+" bukan bilangan prima");
+//        }
+//    }
     }
 }
