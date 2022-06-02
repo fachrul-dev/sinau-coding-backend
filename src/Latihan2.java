06c5381102c1521835c9aa23d4c35939e381d597
fachrul-dev <fachrul.dev99@gmail.com>
2022-06-02 16:45:26 +0700

    latihan 2

--- src/Latihan2.java
+++ src/Latihan2.java
@@ -1,6 +1,17 @@
 public class Latihan2 {
 
     public static void main(String[] args) {
-//        Tes
+        for (int i=1; i<=20; i++) {
+            int b =0;
+            for (int j = 1; j <= i; j++){
+                if (i % j == 0)
+                    b = b + 1;
+            }
+            if (b == 2) {
+                System.out.println(i+ " bilangan prima ");
+            }else {
+                System.out.println(i+ " bukan bilangan prima");
+            }
+        }
     }
 }
